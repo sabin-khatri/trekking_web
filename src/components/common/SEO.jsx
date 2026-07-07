bf0c252b3aa2195b5eea738e9f18f0714108edb0
sabin khatri <khatrisabin611@gmail.com>
2026-07-07 21:24:17 +0545

    updated code

--- src/components/common/SEO.jsx
+++ src/components/common/SEO.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { COMPANY } from '../../config/company';
+import { IMAGES } from '../../config/images';
 
-export default function SEO({ title, description, type = 'website', image = '//img/everest.jpg' }) {
-  const siteName = "Himalayan Treks & Adventures";
+export default function SEO({ title, description, type = 'website', image = IMAGES.og }) {
+  const siteName = COMPANY.name;
   const fullTitle = title === siteName ? title : `${title} | ${siteName}`;
-  const defaultDescription = "Experience the breathtaking beauty of Nepal with our expertly guided trekking packages to Everest, Annapurna, and beyond.";
+  const defaultDescription = COMPANY.description;
 
   return (
     <Helmet>
-      {/* Standard metadata tags */}
       <title>{fullTitle}</title>
       <meta name="description" content={description || defaultDescription} />
 
-      {/* OpenGraph tags for Facebook/LinkedIn */}
       <meta property="og:type" content={type} />
+      <meta property="og:site_name" content={siteName} />
       <meta property="og:title" content={fullTitle} />
       <meta property="og:description" content={description || defaultDescription} />
       <meta property="og:image" content={image} />
 
-      {/* Twitter tags */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={fullTitle} />
       <meta name="twitter:description" content={description || defaultDescription} />
